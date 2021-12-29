54d8a830ba520aeff4fd9cc5546f968c16200a17
Brahma Keerthi <brahmakeerthihs008@gmail.com>
2021-12-29 22:22:04 +0530

    chapter 1 committed

---
+++ chap 1/1_twincle.py
@@ -0,0 +1,6 @@
+# This will print the twincle twincle little star poem
+
+print('''Twincle twinkle little star 
+how i wonder what you are
+up above the world whats so so high
+like a diamond in the sky''')
---
+++ chap 1/music.py
@@ -0,0 +1,12 @@
+# this program is music player
+from playsound import playsound
+
+print("PRESS 1 for hogbutta charles\nPRESS 2 for kotigobba title track")
+n = int(input())
+
+if n==1 :
+    playsound('E:\\SONGS\\hogbuta charles.mp3')
+
+elif n==2:
+    playsound('E:\\SONGS\\kotigobba title.mp3')
+
